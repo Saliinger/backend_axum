5733173dbbdf4947d6d4cdd8be4af38d6788437e
Saliinger <alexisnoukan@gmail.com>
2026-02-17 16:24:14 +0100

    init

---
+++ src/api/mod.rs
@@ -0,0 +1,5 @@
+use axum::Router;
+
+public async fn get_router() -> Router {
+	Router:new()
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use backend_axum::api;
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
